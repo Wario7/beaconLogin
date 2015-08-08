d7116a08137f2c1e927c6c07a19b9d2b7f10fb84
Wario7 <ahomaidcmuq@gmail.com>
2015-08-08 20:04:51 +0300

    firstCommit

---
+++ app/src/main/java/project/beacon/FeedReaderContract.java
@@ -0,0 +1,21 @@
+package project.beacon;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Aziz on 8/7/2015.
+ */
+public final class FeedReaderContract {
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    public FeedReaderContract() {}
+
+    /* Inner class that defines the table contents (column names) */
+    public static abstract class FeedEntry implements BaseColumns {
+        public static final String TABLE_NAME = "users";
+//        public static final String COLUMN_NAME_ENTRY_ID = "entryid";
+        public static final String COLUMN_NAME_EMAIL = "email";
+        public static final String COLUMN_NAME_PASSWORD = "password";
+
+    }
+}
